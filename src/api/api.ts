d55645980294480d0abe51284313a92cb2e5b31c
Tair Hasandjanov <tair.hasandjanov@gmail.com>
2021-05-31 23:20:12 +0400

    axios classnames formik react-router-dom typescript reselect redux-thunk redux

---
+++ src/api/api.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { UserType } from '../types/types';
+
+export const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "02c10def-b947-4a32-81cd-f27a7ec5db73"
+    }
+});
+
+export enum ResultCodesEnum {
+    Success = 0,
+    Error = 1
+}
+
+export enum ResultCodeForCapcthaEnum {
+    CaptchaIsRequired = 10
+}
+
+export type GetItemsType = {
+    items: Array<UserType>
+    totalCount: number
+    error: string | null
+}
+export type APIResponseType<D = {}, RC = ResultCodesEnum> = {
+    data: D
+    messages: Array<string>
+    resultCode: RC
+}
